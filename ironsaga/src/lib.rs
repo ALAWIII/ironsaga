f704e3184de97dec591632cca0ae7d4a6af8e7f6
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-03-18 17:03:20 +0300

    feat: added ironsaga core structs, enums and Command traits.
    - implemented IronSaga* structs execute_all/ rollback_all functionalites.

---
+++ ironsaga/src/lib.rs
@@ -0,0 +1,78 @@
+pub use anyhow;
+use async_trait::async_trait;
+pub use ironsaga_macros::ironcmd;
+#[async_trait]
+pub trait AsyncCommand {
+    async fn execute(&mut self) -> anyhow::Result<()>;
+    async fn rollback(&mut self) -> anyhow::Result<()>;
+}
+pub trait SyncCommand {
+    fn execute(&mut self) -> anyhow::Result<()>;
+    fn rollback(&mut self) -> anyhow::Result<()>;
+}
+pub enum CommandKind<'a> {
+    AsyncCmd(Box<dyn AsyncCommand + 'a>),
+    SyncCmd(Box<dyn SyncCommand + 'a>),
+}
+pub struct IronSagaAsync<'a, T> {
+    commands: Vec<CommandKind<'a>>,
+    context: Option<T>,
+}
+impl<'a, T> IronSagaAsync<'a, T> {
+    pub async fn execute_all(&mut self) -> anyhow::Result<()> {
+        for (i, v) in self.commands.iter_mut().enumerate() {
+            let r = match v {
+                CommandKind::AsyncCmd(c) => c.execute().await,
+                CommandKind::SyncCmd(c) => c.execute(),
+            };
+            if let Err(er) = r {
+                self.rollback_all(i).await?;
+                return Err(er);
+            }
+        }
+        Ok(())
+    }
+    async fn rollback_all(&mut self, index: usize) -> anyhow::Result<()> {
+        for i in (0..index).rev() {
+            match self.commands.get_mut(i).unwrap() {
+                CommandKind::AsyncCmd(c) => c.rollback().await,
+                CommandKind::SyncCmd(c) => c.rollback(),
+            }?;
+        }
+        Ok(())
+    }
+    pub fn add_async_command(&mut self, c: impl AsyncCommand + 'a) {
+        let ac = CommandKind::AsyncCmd(Box::new(c));
+        self.commands.push(ac);
+    }
+    pub fn add_sync_command(&mut self, c: impl SyncCommand + 'a) {
+        let ac = CommandKind::SyncCmd(Box::new(c));
+        self.commands.push(ac);
+    }
+}
+
+pub struct IronSagaSync<'a, T> {
+    commands: Vec<Box<dyn SyncCommand + 'a>>,
+    context: Option<T>,
+}
+impl<'a, T> IronSagaSync<'a, T> {
+    pub fn execute_all(&mut self) -> anyhow::Result<()> {
+        for (i, v) in self.commands.iter_mut().enumerate() {
+            let r = v.execute();
+            if let Err(er) = r {
+                self.rollback_all(i)?;
+                return Err(er);
+            }
+        }
+        Ok(())
+    }
+    fn rollback_all(&mut self, index: usize) -> anyhow::Result<()> {
+        for i in (0..index).rev() {
+            self.commands.get_mut(i).unwrap().rollback()?;
+        }
+        Ok(())
+    }
+    pub fn add_sync_command(&mut self, c: impl SyncCommand + 'a) {
+        self.commands.push(Box::new(c));
+    }
+}
